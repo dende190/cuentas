0670d9f9730005bdb23af1556e1d20c3478441dc
Juan Pablo Arnedo <juan.arnedo@zalvadora.com>
2022-02-26 11:22:27 -0500

    agregar ruta en app.js de deuda

--- back/app.js
+++ back/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const {config} = require('./config/config');
+const billsRoute = require('./routes/bills');
 const usersRoute = require('./routes/users');
 const debtorRoute = require('./routes/debtor');
 
@@ -12,10 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors());
-app.use('/images', express.static(__dirname + '/assets/images'));
 
 //Routes
 usersRoute(app);
+billsRoute(app);
 debtorRoute(app);
 
 app.listen(process.env.PORT, () => {
