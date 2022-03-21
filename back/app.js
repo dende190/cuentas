9e8f4a2b1a61a32a865e5d02f9c1a74399004d73
Juan Pablo Arnedo <juan.arnedo@zalvadora.com>
2022-03-21 17:11:48 -0500

    definir el CORS de la apliacion

--- back/app.js
+++ back/app.js
@@ -13,7 +13,9 @@ const analyticsRoute = require('./routes/analytics');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: config.cors,
+}));
 
 //Routes
 usersRoute(app);
